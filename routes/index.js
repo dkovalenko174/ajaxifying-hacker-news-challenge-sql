d702da5c23c9b64cda1ca670bb20ad24ac85a626
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-10-04 18:31:28 +0300

    version 1.0

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { Post } = require('../db/models');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.redirect('/posts');
+});
+
+router.get('/posts', async function (req, res) {
+  let posts = await Post.findAll({ order: [['id', 'DESC']]});
+  res.render('index', { posts });
+});
+
+router.post('/posts/:id/vote', async function (req, res) {
+  const findPost = await Post.findOne({where:{id:req.params.id}});
+  await findPost.increment('votes', { by: 1 });
+  res.redirect('/posts');
+});
+
+router.delete('/:id', async function (req, res, next) {
+  // Создайте здесь логику для удаления постов
+});
+
+router.post('/posts', async function (req, res) {
+   await Post.create({ title: req.body.title, username: 'User', commentCount: Math.floor(Math.random() * 1000) });
+  res.redirect('/posts');
+});
+
+router.get('/posts/:id', async function (req, res) {
+  const post = await Post.findOne({where:{id:req.params.id}});
+  res.render('post', { post });
+});
+
+module.exports = router;
